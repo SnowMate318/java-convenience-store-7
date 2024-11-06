7d0555761769e78f719ccc141e4dc35ced43fa62
taehyun <xogus5070318@gmail.com>
2024-11-06 15:33:41 +0900

    test(FileLoaderTest): 파일 읽기 기능 구현 예외 테스트 구현
    파일 읽어오기 동작의 실패 여부를 쉽게 확인할 수 있도록
    파일 읽어오기를 실패할 경우 IOException과 에러메세지를 출력하는지 확인하는 테스트를 구현하였습니다.
    
    BREAKING CHANGE: None

--- src/test/java/store/FileLoaderTest.java
+++ src/test/java/store/FileLoaderTest.java
@@ -1,4 +1,44 @@
 package store;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("파일 읽기 테스트")
 public class FileLoaderTest {
+
+    private static final String ERROR_TEXT = "[ERROR]";
+
+    @Test
+    public void 재고_파일_읽기_중_에러_메세지_출력_여부_확인(){
+
+        FileLoader fileLoader = new FileLoader();
+        fileLoader.loadProduct();
+        String output = getOutput();
+        assertThat(output).doesNotContain(ERROR_TEXT);
+
+    }
+
+    @Test
+    public void 프로모션_파일_읽기_중_에러_메세지_출력_여부_확인(){
+
+        FileLoader fileLoader = new FileLoader();
+        fileLoader.loadPromotion();
+        String output = getOutput();
+        assertThat(output).doesNotContain(ERROR_TEXT);
+
+    }
+
+    private String getOutput() {
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+        return outputStream.toString();
+    }
+
 }
+
